55941036117592076507d5563fc1ef368ae2b02f
Xavier <52470412+xvrbnv@users.noreply.github.com>
2022-12-01 17:12:11 -0600

    Update python_hello.py

--- python_hello.py
+++ python_hello.py
@@ -2,6 +2,6 @@ print("Hello Mr. Jenkins")
 print("Hello again Mr. Jenkins, your webhook is speaking.")
 secretCode = "123456789"
 
-f = open("secretCode.txt", "w")
+f = open("../secretCode.txt", "w")
 f.write(secretCode)
 f.close()
